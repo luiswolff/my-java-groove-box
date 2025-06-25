12eec09e01108bbd0a4e311e37d581edb14a37d0
luiswolff <luis-manuel.wolff@outlook.com>
2025-06-25 16:45:56 +0200

    separated sound api in SoundService

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,47 +1,9 @@
-import javax.sound.midi.InvalidMidiDataException;
-import javax.sound.midi.MidiEvent;
-import javax.sound.midi.MidiSystem;
-import javax.sound.midi.Sequence;
-import javax.sound.midi.Sequencer;
-import javax.sound.midi.ShortMessage;
-import javax.sound.midi.Track;
+import java.util.List;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-		Sequencer sequencer = MidiSystem.getSequencer();
-		sequencer.open();
-		Sequence sequence = new Sequence(Sequence.PPQ, 4);
-		Track track = sequence.createTrack();
-		addEvent(track, ShortMessage.PROGRAM_CHANGE, 1, 0);
-		addTick(track, 0);
-		addTick(track, 3);
-		addTick(track, 4);
-		addTick(track, 6);
-		addTick(track, 8);
-		addTick(track, 11);
-		addTick(track, 12);
-		addTick(track, 14);
-		addEvent(track, ShortMessage.PROGRAM_CHANGE, 1, 16);
-		sequencer.setSequence(sequence);
-		sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
-		sequencer.start();
-		sequencer.setTempoInBPM(94.0f);
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			if (sequencer.isOpen()) {
-				sequencer.stop();
-				sequencer.close();
-			}
-		}));
-	}
-
-	private static void addTick(Track track, int tick) throws InvalidMidiDataException {
-		addEvent(track, ShortMessage.NOTE_ON, 35, tick);
-		addEvent(track, ShortMessage.NOTE_OFF, 35, tick + 1);
-	}
-
-	private static void addEvent(Track track, int type, int num, int tick) throws InvalidMidiDataException {
-		ShortMessage message = new ShortMessage();
-		message.setMessage(type, 9, num, 100);
-		track.add(new MidiEvent(message, tick));
+		SoundService soundService = new SoundService();
+		soundService.play(new Beat(List.of(0, 3, 4, 6, 8, 11, 12, 14)));
+		Runtime.getRuntime().addShutdownHook(new Thread(soundService::stop));
 	}
 }
---
+++ src/main/java/SoundService.java
@@ -0,0 +1,50 @@
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+class SoundService {
+	final Sequencer sequencer;
+	final Sequence sequence;
+	final Track track;
+	SoundService() throws Exception {
+		sequencer = MidiSystem.getSequencer();
+		sequence = new Sequence(Sequence.PPQ, 4);
+		track = sequence.createTrack();
+	}
+
+	void play(Beat beat) throws Exception {
+		addEvent(ShortMessage.PROGRAM_CHANGE, 1, 0);
+		for (Integer tick : beat.ticks()) {
+			addTick(tick);
+		}
+		addEvent(ShortMessage.PROGRAM_CHANGE, 1, 16);
+
+		sequencer.setSequence(sequence);
+		sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
+		sequencer.open();
+		sequencer.setTempoInBPM(94.0f);
+		sequencer.start();
+	}
+
+	void stop() {
+		if (sequencer.isOpen()) {
+			sequencer.stop();
+			sequencer.close();
+		}
+	}
+
+	private void addTick(int tick) throws InvalidMidiDataException {
+		addEvent(ShortMessage.NOTE_ON, 35, tick);
+		addEvent(ShortMessage.NOTE_OFF, 35, tick + 1);
+	}
+
+	private void addEvent(int type, int num, int tick) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(type, 9, num, 100);
+		track.add(new MidiEvent(message, tick));
+	}
+}

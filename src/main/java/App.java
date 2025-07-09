8b665f8aae7be773f962b889a9d04719a366db30
luiswolff <luis-manuel.wolff@outlook.com>
2025-07-09 16:38:22 +0200

    refactored model

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,40 @@
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) throws Exception {
 		SoundService soundService = new SoundService();
-		soundService.play(new Beat(List.of(0, 3, 4, 6, 8, 11, 12, 14)));
+		QuarterNote note1 = new QuarterNote(new Tick(Instrument.ACOUSTIC_BASS_DRUM, 120),
+				null,
+				null,
+				new Tick(Instrument.ELECTRIC_SNARE, 100));
+		QuarterNote note2 = new QuarterNote(new Tick(Instrument.ACOUSTIC_BASS_DRUM, 120),
+				null,
+				new Tick(Instrument.ELECTRIC_SNARE, 100),
+				null);
+		soundService.play(new Beat(List.of(new FourBarPhrase(
+				note1,
+				note2,
+				note1,
+				note2
+		))));
+
+		System.out.println("Press any key to start");
+		new Scanner(System.in).nextLine();
+		soundService.start();
+
+		long lastTick = -1;
+		while (soundService.isRunning()) {
+			long currentTick = soundService.getTickPosition();
+			if (lastTick != currentTick) {
+				String passedTicks = String.join("", Collections.nCopies((int) currentTick, "#"));
+				String missingTicks = String.join("", Collections.nCopies(16 - (int) currentTick, " "));
+				System.out.print("\r" + passedTicks + missingTicks);
+				lastTick = currentTick;
+			}
+		}
+
 		Runtime.getRuntime().addShutdownHook(new Thread(soundService::stop));
 	}
 }
--- src/main/java/SoundService.java
+++ src/main/java/SoundService.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
@@ -17,14 +19,22 @@ class SoundService {
 	}
 
 	void play(Beat beat) throws Exception {
-		for (Integer tick : beat.ticks()) {
-			addTick(tick);
+		List<Tick> ticks = beat.ticks();
+		for (int i = 0; i < ticks.size(); i++) {
+			Tick tick = ticks.get(i);
+			if (tick == null) continue;
+			addEvent(ShortMessage.NOTE_ON, tick, i + 1);
+			addEvent(ShortMessage.NOTE_OFF, tick, i + 2);
 		}
+		addEvent(ShortMessage.PROGRAM_CHANGE, null, ticks.size());
 
 		sequencer.setSequence(sequence);
 		sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
 		sequencer.open();
 		sequencer.setTempoInBPM(94.0f);
+	}
+
+	void start() {
 		sequencer.start();
 	}
 
@@ -35,14 +45,17 @@ class SoundService {
 		}
 	}
 
-	private void addTick(int tick) throws InvalidMidiDataException {
-		addEvent(ShortMessage.NOTE_ON, 35, tick + 1);
-		addEvent(ShortMessage.NOTE_OFF, 0, tick + 2);
+	private void addEvent(int type, Tick tick, int tickVal) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage();
+		message.setMessage(type, 9, tick != null ? tick.instrument().value : 0, tick != null ? tick.velocity() : 0);
+		track.add(new MidiEvent(message, tickVal));
 	}
 
-	private void addEvent(int type, int num, int tick) throws InvalidMidiDataException {
-		ShortMessage message = new ShortMessage();
-		message.setMessage(type, 9, num, 100);
-		track.add(new MidiEvent(message, tick));
+	public boolean isRunning() {
+		return sequencer.isRunning();
+	}
+
+	public long getTickPosition() {
+		return sequencer.getTickPosition();
 	}
 }

6794bd0cdb8d87649be227a2e038ff19a9d49165
luiswolff <luis-manuel.wolff@outlook.com>
2025-07-11 16:25:18 +0200

    switched to JavaFx FXML

--- src/main/java/groovebox/ui/UserInterfaceApp.java
+++ src/main/java/groovebox/ui/UserInterfaceApp.java
@@ -1,17 +1,15 @@
 package groovebox.ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class UserInterfaceApp extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		Label label = new Label("Welcome to Groove");
-		StackPane root = new StackPane(label);
-		Scene scene = new Scene(root, 400, 200);
+	public void start(Stage primaryStage)  throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 400, 200);
 		primaryStage.setTitle("Groove");
 		primaryStage.setScene(scene);
 		primaryStage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module groovebox {
 	requires java.desktop;
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.fxml;
 
+	opens groovebox.ui to javafx.fxml;
 	exports groovebox.ui;
 }
